3e00ec2972f6708155a8e097f47ae0dbc79c10cc
Alan Wang <alan098555@gmail.com>
2021-12-02 19:15:28 +1300

    Debounced Input with Lodash and React useMemo Hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import React, { useState, useEffect, useCallback, useMemo } from 'react'
+import debounce from 'lodash.debounce';
 import './App.css';
 
-function App() {
+function App()
+{
+  const [input, setInput] = useState( '' )
+
+  const fetchData = ( input ) =>
+  {
+    if ( input !== '' )
+    {
+      console.log( `Searching For ${ input }` )
+    }
+  }
+
+  const onChange = ( e ) =>
+  {
+    let value = e.target.value
+    setInput( value )
+    fetchData( value )
+  }
+
+  //useCallback makes sure to return the SAME INSTANCE of the debounced callback between re-renderings
+  // const debouncedInput = useCallback( debounce( onChange, 1000 ), [] )
+
+  //useMemo() calls debounce() just ONCE, while useCallback() during EVERY RE-RENDERING. 
+  //If you look at the debounce() source code, it's a quite EXPENSIVE CALL.
+  const debouncedInput = useMemo( () => debounce( onChange, 1000 ), [] )
+
+  //Stop the invocation of the debounced function after unmounting
+  useEffect( () =>
+  {
+    debouncedInput.cancel()
+  }, [] )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello</h1>
+      <input type="text" aria-label="Search" onChange={debouncedInput} />
     </div>
   );
 }
